2436567454d00d5be3fe7b2d684b122b3188d7cd
ArielZ97 <163203394+ArielZ97@users.noreply.github.com>
2024-05-31 14:00:50 -0300

    Subi los archivos

---
+++ script.js
@@ -0,0 +1,9 @@
+const socialMediaLinks = document.querySelectorAll('#social-media-links a');
+
+// Replace the href attributes with your actual social media links
+socialMediaLinks[0].href = 'https://www.facebook.com/your-page';
+socialMediaLinks[1].href = 'https://www.twitter.com/your-handle';
+socialMediaLinks[2].href = 'https://www.instagram.com/your-handle';
+
+
+
